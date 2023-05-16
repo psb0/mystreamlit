c0cfb860554214791a055d12568828de6496f4f2
박수빈 <122463558+psb0@users.noreply.github.com>
2023-05-16 12:52:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import numpy as np
 
 st.title('The title')
 st.header('The header')
-df = pd.DataFrame(np.random.random(500,2) / [50, 50] + [37.76, -122.4], columns = ['lat', 'lon'])
+df = pd.DataFrame(np.random.randn(500,2) / [50, 50] + [37.76, -122.4], columns = ['lat', 'lon'])
 st.map(df)
